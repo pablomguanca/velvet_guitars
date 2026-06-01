2e7925307a06d8273e9860862d711a7049873e6e
PabloGuanca <pablo.m.guanca@gmail.com>
2026-06-01 15:11:27 -0300

    feat: maquetación y estilo de navbar sticky + loader

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logoVelvet from '../assets/guitar_ecommerce_logo.png';
 import CartWidget from './CartWidget';
-import logoRSkate from '../assets/logo_rollerskate!.png';
 
-const Navbar =()=>{
+const NavBar = () => {
     return (
-        <nav className="nav-container">
-            <a href=""><img src={logoRSkate} alt="logo"/></a>
-            <a href="">Productos</a> 
-            <a href="">Novedades</a>
-            <a href="">Hot Sale</a>
-            <a href="">Nosotros</a>
-            <a href="">Mi Cuenta</a>
-            <CartWidget/>
-        </nav>
+        <Navbar expand="lg" className="velvet-navbar">
+            <Container fluid>
+                <Navbar.Brand href="#">
+                    <img className="logo-header" src={logoVelvet} alt="logo" />
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="navbarScroll" />
+
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav className="ms-auto my-2 my-lg-0 align-items-center">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#productos">Productos</Nav.Link>
+                        <Nav.Link href="#nosotros">Nosotros</Nav.Link>
+                        <Nav.Link href="#contacto">Contacto</Nav.Link>
+                        <CartWidget />
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     )
 }
 
-export default Navbar
+export default NavBar

8e606d9ad4231aaadfccbfc91e9667ade85ee595
PabloGuanca <pablo.m.guanca@gmail.com>
2026-06-03 21:45:38 -0300

    feat: configurar react-router y agregar navegación al Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import ItemCount from './components/ItemCount';
 import ItemListContainer from './components/ItemListContainer';
 import Navbar from './components/Navbar';
-import { Route, Routes } from 'react-router-dom';
-import Toggle from './components/Toggle';
+import ItemDetailContainer from './components/ItemDetailContainer';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import CartContainer from './components/CartContainer';
+
 function App() {
+
+  console.log('Hola soy App')
   return (
-    <>
-    <Navbar/>
-      <h1>Hi, guys!</h1>
-      <Routes>
-      {/* <Route path='/' element = p></Route> */}
-      </Routes>
-      <ItemCount/>
-      <ItemListContainer saludo = 'Holaaa!'/>
-      <Toggle/>
-    </>
+    <BrowserRouter>
+      <CartProvider>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<ItemListContainer saludo='Bienvenidos a Velvet Guitars!'/>} />
+          <Route path='/category/:type' element={<ItemListContainer saludo='Categoria ' />} />
+          <Route path='/item/:id' element={<ItemDetailContainer />} />
+          <Route path='/cart' element={<CartContainer />} />
+        </Routes>
+      </CartProvider>
+    </BrowserRouter>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,12 +3,13 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logoVelvet from '../assets/guitar_ecommerce_logo.png';
 import CartWidget from './CartWidget';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
         <Navbar expand="lg" className="velvet-navbar">
             <Container fluid>
-                <Navbar.Brand href="#">
+                <Navbar.Brand as={Link} to="/">
                     <img className="logo-header" src={logoVelvet} alt="logo" />
                 </Navbar.Brand>
 
@@ -16,10 +17,10 @@ const NavBar = () => {
 
                 <Navbar.Collapse id="navbarScroll">
                     <Nav className="ms-auto my-2 my-lg-0 align-items-center">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#productos">Productos</Nav.Link>
-                        <Nav.Link href="#nosotros">Nosotros</Nav.Link>
-                        <Nav.Link href="#contacto">Contacto</Nav.Link>
+                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/category/nuevos">Productos</Nav.Link>
+                        <Nav.Link as={NavLink} to="/nosotros">Nosotros</Nav.Link>
+                        <Nav.Link as={NavLink} to="/contacto">Contacto</Nav.Link>
                         <CartWidget />
                     </Nav>
                 </Navbar.Collapse>
